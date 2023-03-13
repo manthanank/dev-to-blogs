220a2724332dcfa36667fe32f1dfae4f26c45b96
Manthan Ankolekar <manthan.ank@gmail.com>
2023-03-13 22:44:28 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { DataService } from './shared/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dev-to-blogs';
+export class AppComponent implements OnInit {
+
+  details: any;
+  
+  name = new FormControl('', Validators.required);
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() { }
+
+  getBlogdetails() {
+    this.dataService
+      .getBlogDetails(this.name.value)
+      .pipe()
+      .subscribe((res) => {
+        console.log(res);
+        this.details = res;
+      });
+  }
 }
